0f7e66aa723bde783651686655b29dbe5aa7b89d
aldair05 <aldair.uaem@gmail.com>
2020-06-19 15:13:54 -0500

    Menufijo

--- js/app.js
+++ js/app.js
@@ -6,5 +6,25 @@ $(function (){
         $('.navegacion-principal').slideToggle();
     });
 
+    //Menu Fijo 
+    var windowHeight = $(window).height();
+    var barraAltura = $('.barra').innerHeight();
+    
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+
+        console.log(scroll);
+        if(scroll > windowHeight){
+            $('.barra').addClass('fixed');
+            $('body').css({'margin-top':barraAltura+'px'});
+
+        }else{
+            $('.barra').removeClass('fixed'); 
+            $('body').css({'margin-top':'0px'});  
+        }
+        
+    });
+
+
 
 });
